fc83ca8cf18c143bdf0aa4a8f897c9ea1543d5b2
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-24 03:12:30 +0100

    added more selectors

--- src/utility/querySelectors.js
+++ src/utility/querySelectors.js
@@ -1,3 +1,6 @@
 export const canvas = document.querySelector("#uploaded-picture-canvas");
 export const uploadPictureInput = document.querySelector("#upload-picture-input");
-
+export const mainColorDiv = document.querySelector('#main-color-div')
+export const rgbColor = document.querySelector('#rgb-color')
+export const hexColor = document.querySelector('#hex-color')
+export const paletteColors = document.querySelectorAll('.palette-colors')
