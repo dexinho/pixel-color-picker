393515d90a0b79161cb73cdb99da793a7487d741
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-24 03:11:54 +0100

    now only calls settings and setup

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-import { CANVAS_DATA } from "./src/utility/globalVar.js";
-import { canvas } from "./src/utility/querySelectors.js";
-import {
-  uploadPicture,
-  showPixelColor,
-  capturePixelColor,
-  removeHighlightedColor,
-} from "./src/utility/uploadPicture.js";
+import { uploadedImageSettings } from "./src/utility/uploadedImageSettings.js";
+import { initialCanvasSetup } from "./src/utility/canvasSetup.js";
 
-canvas.width = CANVAS_DATA.width;
-canvas.height = CANVAS_DATA.width;
-
-uploadPicture();
-canvas.addEventListener("mousemove", showPixelColor);
-canvas.addEventListener("mouseout", removeHighlightedColor);
-canvas.addEventListener("click", capturePixelColor);
+initialCanvasSetup();
+uploadedImageSettings();
