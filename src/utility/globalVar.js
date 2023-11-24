fd220989349dc9c35de5ea3c5c3b234112c47d16
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-24 03:12:15 +0100

    global var now contains more data

--- src/utility/globalVar.js
+++ src/utility/globalVar.js
@@ -1,12 +1,12 @@
 export const IMAGE_DATA = {
   image: "",
   pixel: "",
-  rgb: {},
+  rgbColors: {},
 };
 
 export const CANVAS_DATA = {
-  width: 0,
-  height: 0,
+  width: 640,
+  height: 360,
   offset: 20,
   ctx: {},
 };
